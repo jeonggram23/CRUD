4b8b7ca3718fccc9eaba4cd0b87b34dec9b6f80c
jeonggram23 <jeonggram23@gmail.com>
2023-08-22 13:47:52 +0900

    Create 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Post 모델을 import
 from .models import Post
 
@@ -22,3 +22,29 @@ def detail(request, id):
     }
 
     return render(request, 'detail.html', context)
+
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    # print(request)
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    # print(title, content)
+
+    post = Post()
+    post.title = title
+    post.content = content
+    # print(post)
+    post.save()
+
+    return redirect(f'/posts/{post.id}/')
+
+
+def delete(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+
+    return redirect('/index/')
